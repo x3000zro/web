4e8fdb8fe076fe3e7a78d86e028461b1cf7e75a6
Jay.tien <jay.tien@hjt-tw.com>
2022-01-14 11:16:44 +0800

    index.php

---
+++ index.php
@@ -0,0 +1,266 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="禾杰通事業有限公司為專業監控系統整合公司，提供了一系列影像傳輸解決方案，專注於安全監控領域，並提供優良穩定的產品。 我們有豐富的經驗和敏銳的判斷力， ...">
+    <meta name="author" content="HJT Ltd.">
+    <meta property="og:title" content="禾杰通 HJT Ltd."></meta>
+    <meta property="og:type" content="website"></meta>
+    <meta property="og:url" content="http://www.hjt-tw.com/"></meta>
+    <meta property="og:image" content="http://www.hjt-tw.com/images/HJT_logo.png"></meta>
+    <meta name="KeyWords" content="HJT,hjt,禾杰通,禾杰通事業有限公司,攝影機,網路攝影機,Synology,Foscam,NAS,switch,IPCAM,PT,PTZ,Bullet,Dome,router,卡巴斯基,Kaspersky"/>
+	<title>禾杰通 HJT Ltd.</title>
+    <base href="http://www.hjt-tw.com/">
+    <!-- LOGO -->
+    <link rel="Shortcut Icon" type="image/x-icon" href="images/HJT.ico" />
+
+	<link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom Fonts -->
+    <link href="vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link href='https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Merriweather:400,300,300italic,400italic,700,700italic,900,900italic' rel='stylesheet' type='text/css'>
+
+    <!-- Plugin CSS -->
+    <link href="vendor/magnific-popup/magnific-popup.css" rel="stylesheet">
+
+	<!-- Theme CSS -->
+    <link href="css/creative.min.css" rel="stylesheet">
+    <link href="css/main.css" rel="stylesheet">
+    <link href="css/style.css" rel="stylesheet">
+    <link href="css/products.css" rel="stylesheet">
+    <link href="css/modern-business.css" rel="stylesheet">
+    <link href="css/banner.css" rel="stylesheet">
+    <link href="css/products_bg.css" rel="stylesheet">
+    <link href="css/hover.css" rel="stylesheet" media="all">
+    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet" media="all">
+
+    <?php include("template/header.php"); ?>
+</head>
+<body>
+
+    <div class="products-content container">
+
+        <div id="myCarousel" class="carousel slide">
+        <!-- Indicators -->
+        <ol class="carousel-indicators">
+            <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+            <li data-target="#myCarousel" data-slide-to="1"></li>
+            <li data-target="#myCarousel" data-slide-to="2"></li>
+            <!-- <li data-target="#myCarousel" data-slide-to="3"></li> -->
+        </ol>
+
+        <!-- Wrapper for slides -->
+        <div class="carousel-inner">
+
+            <div class="item active">
+                <div class="fill banner-2">
+                    <div class="banner-content">
+                        <div class="col-sm-12 col-lg-12 col-md-12 ">
+                            <div class="banner-txt color-w">
+                                <h1>HJT ICD410</h1>
+                            </div>
+                        </div>
+                        <div class="col-sm-12 col-lg-12 col-md-12 ">    
+                            <div class="banner-btn-content">
+                                <a style="text-decoration: none;" href="products_page/camera/ICD410.php">
+                                    <div class="banner-btn font-s-18">
+                                        查看詳情
+                                    </div>
+                                </a> 
+                            </div>
+                        </div>
+                    </div>        
+                </div>
+                <div class="carousel-caption">
+                </div>
+            </div>
+
+            <div class="item">
+                <div class="fill banner">
+                    <div class="banner-content">
+                        <div class="col-sm-12 col-lg-12 col-md-12 ">
+                            <div class="banner-txt">
+                                <h1>Synology RackStation RS4017xs+</h1>
+                            </div>
+                        </div>
+                        <div class="col-sm-12 col-lg-12 col-md-12 ">    
+                            <div class="banner-btn-content">
+                                <a style="text-decoration: none;" href="products_page/nas/RS4017xs+.php">
+                                    <div class="banner-btn font-s-18">
+                                        查看詳情
+                                    </div>
+                                </a> 
+                            </div>
+                        </div>    
+                        <div class="banner-img-content">
+                            <div class="col-sm-12 col-lg-12 col-md-12 ">
+                                <div class="col-sm-12 col-lg-6 col-md-12 ">
+                                    <div class="banner-img">
+                                        <img src="images/banner/syno.png" alt="">
+                                    </div>
+                                </div>
+                                <div class="col-sm-12 col-lg-6 col-md-12 ">
+                                    <div class="banner-img">
+                                        <img src="images/syno/products/RS4017xs+.png" alt="">
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="banner-img-hidden">
+                            <div class="col-sm-12 col-lg-12 col-md-12 ">
+                                <div class="banner-img">
+                                    <img src="images/banner/syno-2.png" alt="">
+                                </div>
+                            </div>
+                        </div>    
+                    </div>    
+                </div>
+                <div class="carousel-caption">
+                </div>
+            </div>
+
+            <div class="item ">
+                <div class="fill banner-1">
+                    <div class="banner-content">
+                        <div class="col-sm-12 col-lg-12 col-md-12 ">
+                            <div class="banner-txt color-2f2f2f">
+                                <h1>Foscam FI9961EP</h1>
+                            </div>
+                        </div>
+                        <div class="col-sm-12 col-lg-12 col-md-12 ">    
+                            <div class="banner-btn-content">
+                                <a style="text-decoration: none;" href="products_page/camera/FI9961EP.php">
+                                    <div class="banner-btn font-s-18">
+                                        查看詳情
+                                    </div>
+                                </a> 
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="carousel-caption">
+                </div>
+            </div>
+            
+        </div>
+
+        <!-- Controls 
+        <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+            <span class="icon-prev"></span>
+        </a>
+        <a class="right carousel-control" href="#myCarousel" data-slide="next">
+            <span class="icon-next"></span>
+        </a>-->
+    </div>
+    <div class="row-1">
+        <div class="col-sm-12 col-lg-12 col-md-12 ">
+            <div class="col-sm-12 col-lg-3 col-md-12 cobox">
+                <div class="thumbnail hvr-glow">
+                    <div class="products-caption caption">   
+                        <div class="title-txt">
+                            <a href="surveillance.php">
+                                <img src="images/index/svs.jpg" alt="">
+                            </a>
+                        </div>
+                        <hr>
+                        <div class="cobox-text">
+                            <h4>網路監控方案</h4>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
+            <div class="col-sm-12 col-lg-3 col-md-12 cobox">
+                <div class="thumbnail hvr-glow">
+                    <div class="products-caption caption">   
+                        <div class="title-txt">
+                            <a href="storage.php">
+                                <img src="images/index/storage.jpg" alt="">
+                            </a>
+                        </div>
+                        <hr>
+                        <div class="cobox-text">
+                            <h4>企業儲存方案</h4>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-sm-12 col-lg-3 col-md-12 cobox">
+                <div class="thumbnail hvr-glow">
+                    <div class="products-caption caption">   
+                        <div class="title-txt">
+                            <a href="backup.php"><img src="images/index/backup.jpg" alt=""></a>
+                        </div>
+                        <hr>
+                        <div class="cobox-text">
+                            <h4>企業備份方案</h4>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-sm-12 col-lg-3 col-md-12 cobox">
+                <div class="thumbnail hvr-glow">
+                    <div class="products-caption caption">   
+                        <div class="title-txt">
+                            <a href="mailplus.php">
+                                <img src="images/index/mail.jpg" alt="">
+                            </a>
+                        </div>
+                        <hr>
+                        <div class="cobox-text">
+                            <h4>郵件伺服器方案</h4>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>           
+    </div>
+
+    <!-- <div class="row-2">
+        <div class="col-sm-12 col-lg-12 col-md-12 ">
+            <div class="thumbnail hvr-glow">
+                
+                    <div class="products-caption">   
+                        <div class="title-txt">
+                            <a href=""><img src="images/index/2.jpg" alt=""></a>
+                        </div>
+                    
+                </div>
+            </div>
+        </div>
+    </div> -->
+</div>    
+
+
+
+<?php include("template/footer.php"); ?>
+
+<!-- jQuery -->
+   <!-- <script type="text/javascript" src="jquery.smartmenus.js"></script>
+    <script src="vendor/jquery/jquery.min.js"></script>  -->
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="vendor/bootstrap/js/bootstrap.min.js"></script> 
+
+    <!-- Plugin JavaScript -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.3/jquery.easing.min.js"></script>
+    <script src="vendor/scrollreveal/scrollreveal.min.js"></script>
+    <script src="vendor/magnific-popup/jquery.magnific-popup.min.js"></script>
+ 
+ <!-- Theme JavaScript -->
+    <script src="js/creative.min.js"></script>
+    <script>
+        $('.carousel').carousel({
+            interval: 5000 //changes the speed
+        })
+    </script>
+</body>
+
+
+</html>
